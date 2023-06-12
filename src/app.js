6961752ff85afa298236f933ac42d22b9acd10b8
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-06-12 14:02:30 -0300

    Adicionada conexão inicial com Moongoose e MongoDB

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 import express from "express";
+import db from "./config/dbConnect.js";
+
+db.on(
+  "error",
+  console.error.bind(console, "Erro de conexão com o banco de dados")
+);
+
+db.once("open", () => {
+  console.log("Banco de dados conectado");
+});
 
 const app = express();
 
