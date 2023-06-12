e3636861cc5bb6244ec2a676879a95f8cd7a9e92
José Víctor Ardasse <josevictorps3lima@gmail.com>
2023-06-12 12:52:23 -0300

    Adicionada a função de UPDATE com verbo HTTP PUT

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const livros = [
+  { id: 1, titulo: "O Senhor dos Anéis", autor: "J. R. R. Tolkien" },
+  { id: 2, titulo: "Harry Potter e a Câmara Secreta", autor: "J. K. Rowling" },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso");
+});
+
+app.put('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+})
+
+function buscaLivro(id) {
+  return livros.findIndex((livro) => livro.id == id);
+}
+
+export default app;
